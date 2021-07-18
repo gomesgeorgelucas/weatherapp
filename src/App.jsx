5e3d477bb3c992bf0cade62278f98fb15a3e9b13
George Lucas Gomes <gomes.georgelucas@gmail.com>
2021-07-18 01:03:14 -0400

    auto load weather data and custom ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import HelloWorld from "./components/HelloWorld";
 import { weatherApi } from "./services/weatherApi";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+  const city = "Amazonas";
+
   async function handleGetWeather() {
-    const response = await weatherApi.get("Curitiba");
+    const response = await weatherApi.get(city);
     console.log(response.data);
+    setWeather(response.data);
   }
+
+  useEffect(() => {
+    handleGetWeather();
+  });
   return (
     <React.Fragment>
       <div className="App">
-        <h1>{"hello world".toUpperCase()}</h1>
-        <HelloWorld></HelloWorld>
+        {/*
+             <header>
+                <button onClick={handleGetWeather}>Enviar</button>
+            </header>
+         */}
+        {weather && (
+          <main>
+            {/* <p>{JSON.stringify(weather)}</p> */}
+            <h1>{city}</h1>
+            <section className="current-weather">
+              <h2> Current Weather</h2>
+              <p>{weather.temperature}</p>
+              <p>{weather.description}</p>
+              <section className="forecast">
+                <h2>Forecast</h2>
 
-        <button onClick={handleGetWeather}>Enviar</button>
+                {weather.forecast.map((day) => (
+                  <li>
+                    <p>{day.temperature}</p>
+                    <p>{day.wind}</p>
+                  </li>
+                ))}
+              </section>
+            </section>
+          </main>
+        )}
       </div>
     </React.Fragment>
   );
